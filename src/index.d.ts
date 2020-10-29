ba17b4c2271aa90ebe4ccd73fc65fac0c527946d
Kunal Patel <kunalp@pinnaclesports.com>
2020-10-29 14:20:41 -0400

    removing empty .d.ts file as its causing component not to render.

--- src/index.d.ts
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// export interface TypingTextProps {
-//     steps: [],
-//     loop?: number,
-//     style?: object | []
-// }
-// declare const TypingText: React.FunctionComponent<TypingTextProps>;
-// export default TypingText
