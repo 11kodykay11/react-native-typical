d9aa8ee6d04d3b41b0b1bbcaf95d7c2cdaf3ce98
Kunal Patel <kunalp@pinnaclesports.com>
2020-10-29 10:25:25 -0400

    Adding type defination

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+import React from "react";
+export interface TypingTextProps {
+    steps: [],
+    loop?: number,
+    style?: object | []
+}
+declare const TypingText: React.FunctionComponent<TypingTextProps>;
+export default TypingText
